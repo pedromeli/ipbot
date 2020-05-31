7613f697204c1f6a5ae2f38cece8852664360336
Pedro Henrique de Jesus Santos <pedrohenrique.santos@mercadolivre.com>
2020-05-31 10:42:49 -0300

    Initial commit

---
+++ src/test/java/io/github/nakassh/ipbot/IpBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nakassh.ipbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
